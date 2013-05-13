9bba7d4d6b45bcc58606f8b5ee38308894f579b0
andrew@chellinsky.com <andrew@chellinsky.com>
2013-05-13 14:25:40 -0700

    Quiz Updates
    Added Quiz to Task Views.  Still need to make fully functional.

--- app/controllers/quizzes/attempts_controller.rb
+++ app/controllers/quizzes/attempts_controller.rb
@@ -14,7 +14,7 @@ class Quizzes::AttemptsController < ApplicationController
     @attempt = @survey.attempts.new(params[:attempt])
     @attempt.participant = current_user
     if @attempt.valid? and @attempt.save
-      redirect_to view_context.new_attempt_path, alert: I18n.t("attempts_controller.#{action_name}")
+      redirect_to space_task_path(2,2), notice: I18n.t("attempts_controller.#{action_name}")
     else
       render :action => :new
     end
--- db/migrate/20130510220119_remove_surveyor_table.rb
+++
@@ -1,8 +0,0 @@
-class RemoveSurveyorTable < ActiveRecord::Migration
-  def up
-  	drop_table :survey_translations
-  end
-
-  def down
-  end
-end
