4b1687897d55ee807746c47911b0f73584557b0f
andrew@chellinsky.com <andrew@chellinsky.com>
2013-05-22 00:16:51 -0700

    Added sendgrid to devise initializer
    Try 3

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,6 +35,9 @@ Gameacert::Application.configure do
   # Expands the lines which load the assets
   config.assets.debug = true
 
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  # config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
+  config.action_mailer.default_url_options = { :host => 'smtp.sendgrid.net' }
+
   
 end
