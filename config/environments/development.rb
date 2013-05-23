709db2d004a82857e42ada88e1cfa2dfe832d9e1
andrew@chellinsky.com <andrew@chellinsky.com>
2013-05-23 12:08:03 -0700

    Locked everything down.
    Other UI tweaks throughout.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,3 +38,14 @@ Gameacert::Application.configure do
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 
 end
+
+ActionMailer::Base.smtp_settings = {
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => 'app15662632@heroku.com',
+  :password       => 'oyavod6v',
+  :domain         => 'heroku.com',
+  :enable_starttls_auto => true
+}
+
